f33faa21680d8fb7a2f0b48d9b69ea1045d91b4a
LS8 <lennartschoettker@hotmail.com>
2017-03-30 22:35:19 +0200

    add condition to check for file and display error message otherwise

--- controllers/file-upload.js
+++ controllers/file-upload.js
@@ -1,8 +1,14 @@
 const fileSize = require('../helpers/file-size');
 module.exports = function(req, res) {
   const info = fileSize(req, res);
-  res.render('size', {
-    message: `The file "${info.name}" consists of ${info.size} bytes`
-  })
+  if (typeof info === "Object") {
+    res.render('size', {
+      message: `The file "${info.name}" consists of ${info.size} bytes`
+    });
+  } else {
+    res.render('size', {
+      message: `${info}`
+    });
+  }
   // res.send(info);
 };
